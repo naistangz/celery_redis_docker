53ac3f8fa84dc2d333cbe4c5dd533dc9231b53c8
AnaisTang <a6anaistang@hotmail.co.uk>
2021-08-02 14:40:56 +0100

    1. Created tasks 2. Fixed typo docker compose 3. Created celery file in app application

--- celery_redis_setup/celery.py
+++ celery_redis_setup/celery.py
@@ -5,3 +5,8 @@ import os
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celery_redis_setup.settings')
+
+app = Celery('celery_redis_setup')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
